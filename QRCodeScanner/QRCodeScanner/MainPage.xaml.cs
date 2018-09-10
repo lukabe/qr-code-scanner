e22b49da1541481b591e71a06366ef574850e21a
Lukas Błaszczyk <blaszczyklukas@gmail.com>
2018-09-11 01:27:45 +0200

    Implement scanner with ZXing

--- QRCodeScanner/QRCodeScanner/MainPage.xaml.cs
+++ QRCodeScanner/QRCodeScanner/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
 
 namespace QRCodeScanner
 {
@@ -13,5 +14,21 @@ namespace QRCodeScanner
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void ScanBtn_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            Navigation.PushAsync(scan);
+            scan.OnScanResult += OnScanResult;
+        }
+
+        private void OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Navigation.PopAsync();
+                returnCodeLbl.Text = result.Text;
+            });
+        }
+    }
 }
